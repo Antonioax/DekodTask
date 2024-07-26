004fc02d5c9517b529e42008b69746a56b3b425c
Antonio Jurić <juricantonio18@gmail.com>
2024-07-26 12:32:47 +0200

    Added employees service

---
+++ src/services/employees.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  constructor(private http: HttpClient) {}
+
+  fetchAllEmployees() {
+    this.http.get(environment.url + 'employees').subscribe({
+      next: (data) => console.log(data),
+    });
+  }
+}
