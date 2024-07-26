0cb1713fae4d317b991dfb3add96a4f34b9ec988
Antonio Jurić <juricantonio18@gmail.com>
2024-07-26 16:42:18 +0200

    Added form component initial design and functionality

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Jobs } from '../../models/jobs';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
-  templateUrl: './form.component.html'
+  imports: [FormsModule, CommonModule],
+  templateUrl: './form.component.html',
 })
 export class FormComponent {
+  allJobs = Jobs;
 
+  formValues = {
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    selectedJob: null,
+  };
+
+  onSubmit(form: NgForm) {
+    console.log('Form Submitted', this.formValues);
+  }
+
+  onReset(form: NgForm) {
+    this.formValues = {
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      selectedJob: null,
+    };
+    form.reset(this.formValues);
+  }
 }
