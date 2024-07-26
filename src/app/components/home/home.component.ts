0bb20e0d1b82c2362b517757e370f5cb300ee661
Antonio Jurić <juricantonio18@gmail.com>
2024-07-26 12:46:32 +0200

    Fetched all employees

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employees.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  allEmployees: any;
+  constructor(private employeeService: EmployeeService) {}
 
+  ngOnInit() {
+    this.allEmployees = this.employeeService.fetchAllEmployees();
+    console.log(this.allEmployees);
+  }
 }
